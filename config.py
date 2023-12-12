ee4ce014f32186b3e17681b92539350ceb16b752
Ilmir <ilmirsharifullin2000@gmail.com>
2023-12-12 15:37:20 +0300

    logs fix

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class CustomLogger(logging.Logger):
 
 
 def setup_logger():
-    logging.basicConfig(level=os.getenv("DEBUG_LEVEL", 10), format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=int(os.getenv("DEBUG_LEVEL", 10)), format='%(asctime)s - %(levelname)s - %(message)s')
 
     return CustomLogger(__name__)
 
