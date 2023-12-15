57190e194f88d24d03006cf9f4036fe6c252dbab
Ilmir <ilmirsharifullin2000@gmail.com>
2023-12-15 13:22:41 +0300

    session replace

--- __main__.py
+++ __main__.py
@@ -42,7 +42,7 @@ async def connect_sessions(*sessions):
 
 async def main():
     channels = ['+EmHan_WSvDdmNDky', '+PG9C65bK6_k3MDI6', '+ie1wo23VFH4xZmYy', 'aznakaevo', '+XsUt7iTl7w8zNTMy', 'bugulma', '+HL12JzSXTAozMjJi']
-    sessions = ['12098898404', '12098898436', '12098898668', '12102692058', '12102738279', '12105738318', '12107103112', '12107103242', '12107103256', '12107106097', '12109565013', '12132348393']
+    sessions = ['12098898404', '12098898436', '12098898668', '12102692058', '12102738279', '12105738318', '12107103112', '12107103242', '12107103256', '12133785331', '12109565013', '12132348393']
     for i in range(len(sessions)):
         proxy_id = i % Proxy.proxy_count
         sessions[i] = (sessions[i], proxy_id, [channels[0], channels[i//2 + 1]])
